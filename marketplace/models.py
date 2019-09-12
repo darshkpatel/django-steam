b25395cb687eb643bf2b7187f2e524c8ca906387
Darsh Patel <darshkpatel@gmail.com>
2019-09-12 09:35:00 +0530

    Add basic models

--- marketplace/models.py
+++ marketplace/models.py
@@ -1,3 +1,35 @@
 from django.db import models
-
+from django.core.validators import MaxValueValidator, MinValueValidator, MinLengthValidator, MaxLengthValidator
+from datetime import date
 # Create your models here.
+class User(models.Model):
+    dob = models.DateField()
+    first_name = models.TextField(max_length=50)
+    last_name = models.TextField(max_length=50)
+    email = models.EmailField()
+    recovered_email = models.EmailField()
+    username = models.TextField(max_length=25, unique=True, primary_key=True)
+    # waller_id = models.ForeignKey
+class Game(models.Model):
+    price = models.DecimalField(default=0.0, decimal_places=1, max_digits=5)
+    gameID = models.AutoField(primary_key=True)
+    timesBought = models.IntegerField(default=0)
+    releaseDate = models.DateField(default=date.today)
+    description = models.TextField()
+    ageRating = models.IntegerField(default=7)
+    stars = models.DecimalField(validators=[MaxValueValidator(5), MinValueValidator(0)], max_digits=1, decimal_places=1)
+    reviews = models.ForeignKey('Review', on_delete=models.CASCADE)
+
+class Review(models.Model):
+    reviewId = models.AutoField(primary_key=True)
+    username = models.ForeignKey('User', on_delete=models.CASCADE)
+    reviewText = models.TextField(validators=[MinLengthValidator(5)])
+class Tag(models.Model):
+    tagId = models.AutoField(primary_key=True)
+    tag = models.TextField(validators=[MaxLengthValidator(10)])
+
+
+
+
+
+    
