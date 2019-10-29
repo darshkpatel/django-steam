398126005c0080b3704e07599449c83c90025841
Darsh Patel <darshkpatel@gmail.com>
2019-10-29 06:25:44 +0530

    PLEASE REVERT: Temporary , to save battery life

--- django_steam/settings.py
+++ django_steam/settings.py
@@ -84,24 +84,24 @@ STATIC_URL = '/static/'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'django_steam',
-        'USER': 'django',
-        'PASSWORD': 'passpass',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'django_steam',
+#         'USER': 'django',
+#         'PASSWORD': 'passpass',
+#         'HOST': 'localhost',
+#         'PORT': '',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
